add1a634934507f930337651e98be8b9b098b7b0
karasu <gustaucastells@gmail.com>
2023-11-22 10:56:09 +0100

    Working

--- challenges/models.py
+++ challenges/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+#class Question(models.Model):
+#    question_text = models.CharField(max_length=200)
+#    pub_date = models.DateTimeField("date published")
+
+
+#class Choice(models.Model):
+#    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#    choice_text = models.CharField(max_length=200)
+#    votes = models.IntegerField(default=0)
+
+
+class Challenge(models.Model):
+    title = models.CharField(max_length=200)
+    creator = models.OneToOneField(
+        Teacher,
+        on_delete=models.CASCADE,
+        primary_key=True) 
+    description = models.TextField()
+    port = models.IntegerField()
+
+class Teacher(models.Model):
+    email = models.CharField(max_length=200)
+    username = models.CharField(max_length=64)
+    avatar = models.ImageField()
+
+class Student(models.Model):
+    email = models.CharField(max_length=200)
+    username = models.CharField(max_length=64)
+    avatar = models.ImageField()
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
--- inabox/settings.py
+++ inabox/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'inabox.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'inabox.sqlite3',
     }
 }
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Rome'
 
 USE_I18N = True
 
