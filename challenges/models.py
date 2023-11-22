87bf4b64cfb392a681714efa96efc7a38fc1b2fd
gustau <gustaucastells@gmail.com>
2023-11-22 16:55:26 +0100

    Working on models

--- challenges/models.py
+++ challenges/models.py
@@ -5,26 +5,6 @@ from django.db import models
 from django.db import models
 
 
-#class Question(models.Model):
-#    question_text = models.CharField(max_length=200)
-#    pub_date = models.DateTimeField("date published")
-
-
-#class Choice(models.Model):
-#    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#    choice_text = models.CharField(max_length=200)
-#    votes = models.IntegerField(default=0)
-
-
-class Challenge(models.Model):
-    title = models.CharField(max_length=200)
-    creator = models.OneToOneField(
-        Teacher,
-        on_delete=models.CASCADE,
-        primary_key=True) 
-    description = models.TextField()
-    port = models.IntegerField()
-
 class Teacher(models.Model):
     email = models.CharField(max_length=200)
     username = models.CharField(max_length=64)
@@ -35,3 +15,21 @@ class Student(models.Model):
     username = models.CharField(max_length=64)
     avatar = models.ImageField()
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+class DockerImage(models.Modlel):
+    name = models.CharField(max_length=64)
+    ssh_port = models.IntegerField()
+    limit = models.IntegerField(default=30)
+    reuse = models.BooleanField(default=False)
+    ports = models.CharField(max_length=256)
+    volumes = models.TextField()
+
+class Challenge(models.Model):
+    title = models.CharField(max_length=200)
+    creator = models.OneToOneField(
+        Teacher(),
+        on_delete=models.CASCADE,
+        primary_key=True) 
+    description = models.TextField()
+    docker_image = models.ForeignKey(DockerImage, on_delete=models.CASCADE)
+
