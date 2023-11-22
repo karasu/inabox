1ff85a0db69d0808e191022eee288c8cc83e6511
karasu <gustaucastells@gmail.com>
2023-11-22 17:02:11 +0100

    More work on models

--- challenges/models.py
+++ challenges/models.py
@@ -4,34 +4,41 @@ from django.db import models
 
 from django.db import models
 
+class Teacher(models.Model):
+    email = models.CharField(max_length=256)
+    username = models.CharField(max_length=64)
+    avatar = models.ImageField()
+    name = models.CharField(max_length=256)
 
-#class Question(models.Model):
-#    question_text = models.CharField(max_length=200)
-#    pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.name
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return "user_{0}/{1}".format(instance.user.id, filename)
 
-#class Choice(models.Model):
-#    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#    choice_text = models.CharField(max_length=200)
-#    votes = models.IntegerField(default=0)
+class User(models.Model):
+    email = models.CharField(max_length=256)
+    username = models.CharField(max_length=64)
+    avatar = models.ImageField(upload_to=user_directory_path)
+    name = models.CharField(max_length=256)
+    group = models.CharField(max_length=32)
+    teacher = models.BooleanField(default=False)
+    def __str__(self):
+        return self.name
+
 
 
 class Challenge(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=256)
     creator = models.OneToOneField(
-        Teacher,
+        User,
         on_delete=models.CASCADE,
         primary_key=True) 
     description = models.TextField()
     port = models.IntegerField()
+    image = models.ImageField()
 
-class Teacher(models.Model):
-    email = models.CharField(max_length=200)
-    username = models.CharField(max_length=64)
-    avatar = models.ImageField()
+    def __str__(self):
+        return self.title
 
-class Student(models.Model):
-    email = models.CharField(max_length=200)
-    username = models.CharField(max_length=64)
-    avatar = models.ImageField()
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
--- inabox/settings.py
+++ inabox/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'challenges.apps.ChallengesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# This needs to be changed in production
+MEDIA_ROOT = '/tmp'
+
