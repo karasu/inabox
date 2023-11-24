ad4bb3a08b3b4c87ae6b652c3d60d6d27f80e7de
karasu <gustaucastells@gmail.com>
2023-11-24 09:59:54 +0100

    Working on

--- challenges/views.py
+++ challenges/views.py
@@ -1,12 +1,11 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-
-# Create your views here.
-
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.urls import reverse
+from django.views import generic
 
 from .models import Challenge
 
+'''
 def index(request):
     latest_challenge_list = Challenge.objects.order_by("-pub_date")[:10]
     context = {"latest_challenge_list": latest_challenge_list}
@@ -16,3 +15,17 @@ def challenge(request, challenge_id):
     challenge = get_object_or_404(Challenge, pk=challenge_id)
     return render(request, "challenges/challenge.html",
                    {"challenge": challenge})
+'''
+
+class IndexView(generic.ListView):
+    template_name = "challenge/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Challenge.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Challenge
+    template_name = "polls/detail.html"
