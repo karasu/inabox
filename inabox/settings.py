4a628b8df5d50bf7e5a02694cba2a2534f02dff0
karasu <gustaucastells@gmail.com>
2023-12-27 17:29:19 +0100

    Working on translation

--- inabox/settings.py
+++ inabox/settings.py
@@ -104,7 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ca-es'
 
 TIME_ZONE = 'Europe/Rome'
 
