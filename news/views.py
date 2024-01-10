fe9a279609c29438f63ce4fe63e1f1bbeaa3035f
karasu <gustaucastells@gmail.com>
2024-01-10 13:13:43 +0100

    working on search form

--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.views import generic
 from django.utils.translation import get_language
 
 from .models import NewsEntry
+from .forms import SearchForm
 from challenges.models import Profile
 
 
@@ -27,3 +28,8 @@ class AboutView(generic.base.TemplateView):
 
 class SearchView(generic.base.TemplateView):
     template_name = "news/search.html"
+
+    def search(self, request, *args, **kwargs):
+        print(request)
+        form = SearchForm({'search': request.get.search})
+
