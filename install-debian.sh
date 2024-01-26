e54f17d7a48be0bc0512ab56c59ff7cfc01ad346
karasu <gustaucastells@gmail.com>
2024-01-26 07:52:46 +0100

    Working on install script (development)

--- install-debian.sh
+++ install-debian.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 sudo apt install python3 \
-python3-celery \
-python3-celery-progress \
-python3-channels-redis \
-python3-daphne \
-python3-django \
-python3-django-celery-results \
-python3-django-channels \
-python3-docker \
-python3-msgpack \
-python3-paramiko \
-python3-pycryptodome \
-python3-redis \
-python3-yaml \
-docker-compose
+    python3-celery \
+    python3-celery-progress \
+    python3-channels-redis \
+    python3-daphne \
+    python3-django \
+    python3-django-celery-results \
+    python3-django-channels \
+    python3-docker \
+    python3-msgpack \
+    python3-paramiko \
+    python3-pycryptodome \
+    python3-pylint-django \
+    python3-redis \
+    python3-yaml \
+    docker-compose
 
 #django-admin-interface
