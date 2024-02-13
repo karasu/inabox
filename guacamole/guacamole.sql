6d80d35ff75ec0ac6bf5dd3e614185859f0d94f8
karasu <gustaucastells@gmail.com>
2024-02-13 13:29:30 +0100

    guacamole is working

--- guacamole/guacamole.sql
+++ guacamole/guacamole.sql
@@ -21,6 +21,12 @@
 -- Connection group types
 --
 
+--CREATE DATABASE guacamole_db;
+--CREATE USER guacamole_user WITH PASSWORD 'guacamole';
+--GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA public TO guacamole_user;
+--GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA public TO guacamole_user;
+
+
 CREATE TYPE guacamole_connection_group_type AS ENUM(
     'ORGANIZATIONAL',
     'BALANCING'
