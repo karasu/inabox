572b67a890d6e76607ceb67530a2ce7ad969b709
karasu <gustaucastells@gmail.com>
2024-11-26 08:51:02 +0100

    fix domain

--- src/inabox/settings.py
+++ src/inabox/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-DOMAIN = "inabox.gustau.cat"
+DOMAIN = "inabox.ies-sabadell.cat"
 
 BOOL_STR = {
     "True": ['true', '1', 'y', 'yes'],
